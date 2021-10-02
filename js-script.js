f1d7053d388b98ff0ec46077c6fa9c16a88bd888
Lera <ohhsith@ukr.net>
2021-10-02 12:44:12 +0300

    fix

--- js-script.js
+++ js-script.js
@@ -1,8 +1,9 @@
-const person = require('./person') ;
-const student = require('./student') ;
-const teacher = require('./teacher') ;
+// const person = require('./person') ;
+// const student = require('./student') ;
+// const teacher = require('./teacher') ;
 
-let student1 = new student.Student('le', 'gender', 26, 'interests')
-let teacher1 = new teacher.Teacher('ololo', 'gender', 36, 'interests')
-console.log(student1, teacher1)
+// let student1 = new student.Student('le', 'gender', 26, 'interests')
+// let teacher1 = new teacher.Teacher('ololo', 'gender', 36, 'interests')
+// console.log(student1, teacher1)
 
+// оставляю код зедсь, т.к. в задании есть пункт "подклчить джс скрипт в индекс."
